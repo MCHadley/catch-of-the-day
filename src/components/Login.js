c4c144ce2018d56ffbcd7d22bead241a350475c3
Michael <michael.c.hadley@gmail.com>
2020-01-09 17:13:30 -0500

    added firebase authentication

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = (props) => (
+  <nav className="login">
+    <h2>Inventory Login</h2>
+    <p>Sign in to manage your stores inventory</p>
+    <button className="facebook" onClick={() => props.authenticate('Facebook')}>Log in with facebook</button>
+    <button className="twitter" onClick={() => props.authenticate('Twitter')}>Log in with twitter</button>
+  </nav>
+)
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+}
+
+export default Login;
